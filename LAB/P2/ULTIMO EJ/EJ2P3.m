dbe9d064053a016b01a1b7c86f9a21d70605f4de
JuanJoCZ <jose.cordoba@edu.uah.es>
2019-10-20 13:14:57 +0200

    RED NEURONAL NARX FUNCIONANDO

--- LAB/P2/ULTIMO EJ/EJ2P3.m
+++ LAB/P2/ULTIMO EJ/EJ2P3.m
@@ -4,16 +4,15 @@ error_margin = 0.01;
 th_0 = 30*pi/180;
 x_0 = 0;
 y_0 = 0;
-draw = 0;
-N = 20;
+N = 1;
 
 E_d_vec=[];
 E_theta_vec=[];
 V_vec=[];
 W_vec=[];
 
-for num = 1:N
-    for num2 = 1:N
+for num = 0:N
+    for num2 = 0:N
         x_0 = num;
         y_0 = num2;
         sim('PositionControlNARX.slx')
@@ -31,31 +30,30 @@ nT = size(inputs, 2);
 inputsc = mat2cell(inputs, 2, ones(nT, 1));
 outputsc = mat2cell(targets, 2, ones(nT, 1));
 
-nNeuronas= 25;
+nNeuronas= 20;
 performanceMejor = 100;
 netElegida = '';
 internal_performanceMejor = 100;
 internal_netElegida = '';
 
-for i=11:11
-%     for j= 1:5
-    net = narxnet(1:2, 1, [i]);
+for i=5:nNeuronas
+    for j= 1:5
+        net = narxnet(1:2, 1, [i]);
 
-    [x,xi,ai,t] = preparets(net,inputsc,{},outputsc);
+        [x,xi,ai,t] = preparets(net,inputsc,{},outputsc);
 
-    net = train(net,x,t,xi,ai);
-    net = closeloop(net);
+        net = train(net,x,t,xi,ai);
+        net = closeloop(net);
 
-    outputs = net(inputs);
-    errors = gsubtract(outputs, targets);
-    performance = perform(net,targets,outputs);
+        outputs = net(inputs);
+        errors = gsubtract(outputs, targets);
+        performance = perform(net,targets,outputs);
         
-%         if (performance < internal_performanceMejor)
-%            internal_netElegida = net;
-%            internal_performanceMejor = performance;
-%         end
-%         display(strcat("Pnueva: ", num2str(performance), " N: ", num2str(i), " Pmejor: ", num2str(internal_performanceMejor)))
-%     end
+        if (performance < internal_performanceMejor)
+           internal_netElegida = net;
+           internal_performanceMejor = performance;
+        end
+    end
     if (performance < performanceMejor)
        netElegida = net;
        performanceMejor = performance;
