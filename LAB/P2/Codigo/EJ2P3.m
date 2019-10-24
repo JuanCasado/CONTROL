cab0a95917b63562fef1f730202731d39948d867
JuanJoCZ <jose.cordoba@edu.uah.es>
2019-10-24 21:11:36 +0200

    ZIP Modificado Terminado

--- LAB/P2/Codigo/EJ2P3.m
+++ LAB/P2/Codigo/EJ2P3.m
@@ -1,6 +1,5 @@
 
 Ts = 100e-3;
-error_margin = 0.01;
 th_0 = 30*pi/180;
 x_0 = 0;
 y_0 = 0;
@@ -30,36 +29,18 @@ nT = size(inputs, 2);
 inputsc = mat2cell(inputs, 2, ones(nT, 1));
 outputsc = mat2cell(targets, 2, ones(nT, 1));
 
-nNeuronas= 20;
-performanceMejor = 100;
-netElegida = '';
-internal_performanceMejor = 100;
-internal_netElegida = '';
+Ne = 11;
 
-for i=5:nNeuronas
-    for j= 1:5
-        net = narxnet(1:2, 1, [i]);
+net = narxnet(1:2, 1, [Ne]);
 
-        [x,xi,ai,t] = preparets(net,inputsc,{},outputsc);
+[x,xi,ai,t] = preparets(net,inputsc,{},outputsc);
 
-        net = train(net,x,t,xi,ai);
-        net = closeloop(net);
+net = train(net,x,t,xi,ai);
+net = closeloop(net);
 
-        outputs = net(inputs);
-        errors = gsubtract(outputs, targets);
-        performance = perform(net,targets,outputs);
-        
-        if (performance < internal_performanceMejor)
-           internal_netElegida = net;
-           internal_performanceMejor = performance;
-        end
-    end
-    if (performance < performanceMejor)
-       netElegida = net;
-       performanceMejor = performance;
-    end
-end
+outputs = net(inputs);
+errors = gsubtract(outputs, targets);
 
-view(netElegida)
-display(performanceMejor)
-gensim(netElegida,Ts);
+
+view(net)
+gensim(net,Ts);
