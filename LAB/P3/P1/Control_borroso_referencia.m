3d11c456d399ffee41bff39414e43db4033ce035
Juan Jose Cordoba <juanjosecordoba@MacBook-Pro-de-JuanJo.local>
2019-11-22 21:29:14 +0100

    PARTE 1 FUNCIONAL

--- LAB/P3/P1/Control_borroso_referencia.m
+++ LAB/P3/P1/Control_borroso_referencia.m
@@ -1,16 +1,15 @@
 %Tiempo de muestreo
 Ts=100e-3;
 error_margin = 0.02;
-w0 = 0;
 x0 = 0;
 y0 = 0;
+w0 = 0;
 
 % Referencia x-y de posicion
-refx=7;
-refy=0;
+refx= -5;
+refy= 0;
 % Ejecutar Simulacion
-%fis_name = 'Control_borroso_oscilaciones.fis';
-fis_name = 'Control_borroso_gausianas.fis';
+fis_name = 'Control_borroso.fis';
 sim('PositionControl_referencia.slx')
 % Mostrar
 x=salida_x.signals.values;
@@ -18,7 +17,7 @@ y=salida_y.signals.values;
 
 figure
 hold on
-axis([-7 7 -7 7])
+axis([-10 10 -10 10])
 plot(x,y)
 plot(refx,refy, 'ob')
 plot(x0,y0, 'or')
--- LAB/P3/P1/Control_borroso_trajectory.m
+++ LAB/P3/P1/Control_borroso_trajectory.m
@@ -1,18 +1,17 @@
 %Tiempo de muestreo
 Ts=100e-3;
+error_margin = 0.01;
 w0 = 0;
-x0 = 0;
+x0 = -5;
 y0 = 0;
-x_0 = x0;
+x_0 = 0;
 y_0 = y0;
 th_0 = w0;
 
 % Ejecutar Simulacion
-fis_name = 'Control_borroso_oscilaciones.fis';
-%fis_name = 'Control_borroso_gausianas.fis';
-%fis_name = 'Control_borroso_con_gaussianas.fis';
+fis_name = 'Control_borroso.fis';
 
-sim('PositionControl_trayectory.slx') 
+sim('PositionControl_trajectory.slx') 
 % Mostrar
 x=salida_x.signals.values;
 y=salida_y.signals.values;
@@ -25,5 +24,5 @@ axis([-10 10 -10 10])
 plot(x,y, '-b')
 plot(x_ref, y_ref, '-r')
 plot(x0,y0, 'or')
-grid on
+plot(x_0,y_0, 'oy')
 hold off
