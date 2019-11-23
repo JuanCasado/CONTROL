728710db8a66676b1c439d849252a02d17fd065a
Juan Jose Cordoba <juanjosecordoba@MacBook-Pro-de-JuanJo.local>
2019-11-23 20:31:27 +0100

    añadir puntos controlador P2

--- LAB/P3/P2/Control_borroso_obstaculo_reference.m
+++ LAB/P3/P2/Control_borroso_obstaculo_reference.m
@@ -4,14 +4,62 @@ error_margin = 0.05;
 w0 = pi/2;
 x0 = 0;
 y0 = 0;
-
+ 
 % Referencia x-y de posicion
 refx=-4;
 refy=-4;
-obsx=-0.5;
-obsy=1.5;
+obsx=3;
+obsy=3;
+
+%%
+% PRUEBAS REALIZADAS PARA EL DOCUMENTO
+% Prueba 1
+% 
+% w0 = 0;
+% x0 = 0;
+% y0 = 0;
+% refx=6;
+% refy=0;
+% obsx=3;
+% obsy=0;
+% 
+% 
+% Prueba 2
+% 
+% w0 = pi/2;
+% x0 = 0;
+% y0 = 0;
+% refx=-4;
+% refy=-4;
+% obsx=-4;
+% obsy=0;
+% 
+% 
+% Prueba 3
+% 
+% w0 = -2*pi/4;
+% x0 = 0;
+% y0 = 0;
+% refx=4;
+% refy=-6;
+% obsx=1;
+% obsy=-2;
+% 
+% 
+% Prueba 4
+% 
+% 
+% w0 = pi/2;
+% x0 = 0;
+% y0 = 0;
+% refx=-4;
+% refy=-4;
+% obsx=-2.25;
+% obsy=2;
+%%
+
 % Ejecutar Simulacion
-fis_name = 'Control_borroso_obstaculo_reglas_2.fis';
+fis_name = 'Control_borroso_rapido_obs.fis';
 sim('PositionControl_referencia_obs.slx')
 % Mostrar
 x=salida_x.signals.values;
