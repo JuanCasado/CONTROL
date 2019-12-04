691702bbb3610b27b6ec6072926aca4bb9aa4faa
JuanJoCZ <jose.cordoba@edu.uah.es>
2019-12-04 16:03:27 +0100

    Parte 1 SCI terminada

--- LAB/P4/code/mandani.m
+++ LAB/P4/code/mandani.m
@@ -4,8 +4,8 @@ controller_name = 'controlador_mandani_3.fis';
 Ts=100e-3;
 vel = 1;
 offset_0 = -1.6000;
-offset_1 = -2.1600;
-offset_2 =  1.8000;
+offset_1 = -0.1000;
+offset_2 =  -0.2600;
 arround_th = 0.1;
 
 % Ejecutar Simulacion
@@ -15,6 +15,8 @@ sim('controlador_mandani.slx')
 %% Mostrar
 pre_format=pre_format.signals.values;
 post_format=post_format.signals.values;
+sonar2=sonar2.signals.values;
+sonar6=sonar6.signals.values;
 %%
 figure
 hold on
