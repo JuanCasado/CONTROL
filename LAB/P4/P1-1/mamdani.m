66cec617ad310f39937c34e402b17348d9215e29
Juan Jose Cordoba <juanjosecordoba@MacBook-Pro-de-JuanJo.local>
2019-12-29 21:59:17 +0100

    cambios jj

--- LAB/P4/P1-1/mamdani.m
+++ LAB/P4/P1-1/mamdani.m
@@ -13,32 +13,18 @@ arround_th = 0.01;
 sim('controlador_mamdani.slx') 
 
 %% Sonars
-sonar0_ = sonar0.signals.values;
-sonar1_ = sonar1.signals.values;
-sonar2_ = sonar2.signals.values;
-sonar3_ = sonar3.signals.values;
-sonar4_ = sonar4.signals.values;
-sonar5_ = sonar5.signals.values;
-sonar6_ = sonar6.signals.values;
-sonar7_ = sonar7.signals.values;
-out=post_format.signals.values;
 
-sonar0_(isinf(sonar0_)) = 5.0;
-sonar1_(isinf(sonar1_)) = 5.0;
-sonar2_(isinf(sonar2_)) = 5.0;
-sonar3_(isinf(sonar3_)) = 5.0;
-sonar4_(isinf(sonar4_)) = 5.0;
-sonar5_(isinf(sonar5_)) = 5.0;
-sonar6_(isinf(sonar6_)) = 5.0;
-sonar7_(isinf(sonar7_)) = 5.0;
 
-e_sonar0_ = sonar0_ - sonar5_;
-e_sonar1_ = sonar1_ - sonar7_;
-e_sonar2_ = sonar4_ - sonar6_;
+e_sonar0_ = e_sonar0.signals.values;
+e_sonar1_ = e_sonar1.signals.values;
+e_sonar2_ = e_sonar2.signals.values;
+out_ = post_format.signals.values;
 
-train = [e_sonar0_ e_sonar1_ e_sonar2_ out];
+e_sonar0_(isinf(e_sonar0_)) = 5.0;
+e_sonar1_(isinf(e_sonar1_)) = 5.0;
+e_sonar2_(isinf(e_sonar2_)) = 5.0;
+
+train = [e_sonar0_ e_sonar1_ e_sonar2_ out_];
 train = double(train);
 
-
-%%
 save train.dat train -ascii
--- LAB/P4/P2-1/sugeno.m
+++ LAB/P4/P2-1/sugeno.m
@@ -1,4 +1,3 @@
-
 rosshutdown
 controller_name = 'controlador_sugeno.fis';
 
@@ -8,6 +7,13 @@ offset_0 = -1.6000;
 offset_1 = -0.1000;
 offset_2 =  -0.2600;
 arround_th = 0.01;
+
+saturation1_high = 0.17;
+saturation1_low = -0.0100;
+saturation2_high = 0.05;
+saturation2_low = -0.05;
+saturation3_high = 0.09;
+saturation3_low = -0.09;
 
 % Ejecutar Simulacion
 sim('controlador_sugeno.slx')
